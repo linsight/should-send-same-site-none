dd0fd4f7ab4d687f6e327034e1dee1aae5e16009
Joe Cheng <joe@rstudio.com>
2020-04-01 15:33:27 -0700

    Monkeypatch res.writeHead instead of res.end
    The middleware would fail if res.write() or res.writeHead() are called, with the error:
    
    Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
    
    This change moves the logic to the last valid moment to make header changes.
    However, note that the writeHead method takes an optional `headers` argument,
    that this code will not fix up. If you want to address that edge case, you could add
    apply headers argument to the request first, then don't send the headers through
    to writeHead().

--- index.js
+++ index.js
@@ -112,8 +112,8 @@ function isUcBrowserVersionAtLeast(major, minor, build, useragent) {
 }
 
 var shouldSendSameSiteNone = function(req, res, next) {
-  var end = res.end;
-  res.end = function() {
+  var writeHead = res.writeHead;
+  res.writeHead = function() {
     var ua = req.get("user-agent");
     var isCompatible = isSameSiteNoneCompatible(ua);
     var cookies = res.get("Set-Cookie");
@@ -129,7 +129,7 @@ var shouldSendSameSiteNone = function(req, res, next) {
       res.set("Set-Cookie", cookies);
     }
 
-    end.apply(this, arguments);
+    writeHead.apply(this, arguments);
   };
   next();
 };
